8acce6c8d1a2c0730a5814a65286e4c99f57c077
vaibhavbohari <vaibhavbohari@gmail.com>
2022-04-10 12:57:14 +0530

    Enter a Valid Mobile Number

--- UserRegistrationProblem/UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/UserRegistrationProblem/Program.cs
@@ -8,15 +8,16 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             
-            Console.WriteLine("Enter Email");
+            Console.WriteLine("Enter Mobile Number");
             string inStr = Console.ReadLine();
 
             
             string firstName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
             string lastName  = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
             string emailId   = @"^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]@[a-zA-Z0-9]{1,10}.[a-zA-Z]{2,10}[.][a-zA-Z]";
+            string mobileNum = @"^[0-9]{2}\s+[6-9]{1}[0-9]{9}$";
             //creating pattern for emailid
-            Regex regex = new Regex(emailId);
+            Regex regex = new Regex(mobileNum);
             Console.WriteLine(regex.IsMatch(inStr));
         }
     }
