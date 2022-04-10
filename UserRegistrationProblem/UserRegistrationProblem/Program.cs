1576f3a31f030055176e509960526d2d67ec7b1b
vaibhavbohari <vaibhavbohari@gmail.com>
2022-04-10 13:30:20 +0530

    Enter Password

--- UserRegistrationProblem/UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/UserRegistrationProblem/Program.cs
@@ -16,10 +16,10 @@ namespace UserRegistration
             string lastName  = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
             string emailId   = @"^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]@[a-zA-Z0-9]{1,10}.[a-zA-Z]{2,10}[.][a-zA-Z]";
             string mobileNum = @"^[0-9]{2}\s+[6-9]{1}[0-9]{9}$";
-            string password = @"[a-zA-Z]{8}$";
-            
+            string password  =  @"[A-Z]{1}[a-zA-Z]{7}$";
+
             //creating pattern for password
-            
+
             Regex regex = new Regex(password);
             Console.WriteLine(regex.IsMatch(inStr));
         }
